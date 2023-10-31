792660c4955a5e529a7c93582db480cc58e19888
Dr. Dillinger <peter.dillinger@srh.de>
2023-10-31 10:03:11 +0100

    Programm wird wiederholt, bis eine 0 für den Bruttobetrag eingegeben worden ist.

--- src/Steuerberechnung.java
+++ src/Steuerberechnung.java
@@ -2,18 +2,22 @@ import java.util.Scanner;
 
 public class Steuerberechnung {
     public static void main(String[] args) {
-        // E(ingabe)
         Scanner eingabe = new Scanner(System.in);
-        System.out.print("Bitte Bruttobetrag eingeben: ");
-        double brutto = eingabe.nextDouble();
-        System.out.print("Bitte Steuersatz eingeben: ");
-        double steuersatz = eingabe.nextDouble();
+        while ( true ) {
+            // E(ingabe)
+            System.out.print("Bitte Bruttobetrag eingeben: ");
+            double brutto = eingabe.nextDouble();
+            if (brutto == 0.0) break;
+            System.out.print("Bitte Steuersatz eingeben: ");
+            double steuersatz = eingabe.nextDouble();
 
-        // V(erarbeitung)
-        double steuerbetrag = brutto * steuersatz / 100;
-        double netto = brutto - steuerbetrag;
+            // V(erarbeitung)
+            double steuerbetrag = brutto * steuersatz / 100;
+            double netto = brutto - steuerbetrag;
 
-        // A(usgabe)
-        System.out.println("Der Nettobetrag ist " + netto);
+            // A(usgabe)
+            System.out.println("Der Nettobetrag ist " + netto);
+        }
+        System.out.println("Vielen Dank und auf Wiedersehen");
     }
 }
